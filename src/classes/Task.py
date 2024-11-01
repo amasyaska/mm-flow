1bb3abebdecfa4ae5a532c9813824e364bcfa587
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-01 11:16:01 +0200

    modified Task: added description and required_tasks field

--- src/classes/Task.py
+++ src/classes/Task.py
@@ -1,10 +1,15 @@
 from abs import ABC
 
 class Task(ABC):
+    '''
+    :field state: represents state of task, 0 means uncompleted, 1 means completed
+    :field required_tasks: list of required subtasks to complete main task
+    '''
 
     @abstractmethod
-    def __init__(self, state='completed'):
+    def __init__(self, state=0):
         self.state = state
+        self.required_tasks = list()
     
     @abstractmethod
     def complete(self):
