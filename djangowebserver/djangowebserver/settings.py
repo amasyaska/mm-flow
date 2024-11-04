83a41265878160fc21d4b14ada573ceb2b292209
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-11-04 14:32:00 +0200

    deploy1

--- djangowebserver/djangowebserver/settings.py
+++ djangowebserver/djangowebserver/settings.py
@@ -141,6 +141,6 @@ filepath
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
